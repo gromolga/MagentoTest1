f07da9b7e3c035f76bb3efcd12e21c4347e902d7
Grom Olga <GromOlga.work@gmail.com>
2018-10-20 14:19:02 +0300

    Created project/default theme and updated grunt config

---
+++ app/design/frontend/Project/Default/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Grom Olga <GromOlga.work@gmail.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Project/Default',
+    __DIR__
+);
